7c4c43fab7475266be490665fcf29e04f92127c1
skor <olek.skoryk@gmail.com>
2021-07-01 07:57:59 -0500

    Change default params in main

--- src/main.rs
+++ src/main.rs
@@ -17,11 +17,11 @@ fn main() {
     }
 
     let dt_from = APP_TZ.ymd(2017, 1, 1).and_hms(0, 0, 0);
-    let dt_to = APP_TZ.ymd(2020, 12, 31).and_hms(23, 59, 59);
+    let dt_to = APP_TZ.ymd(2021, 12, 31).and_hms(23, 59, 59);
 
     wallet.print_trades(true, &dt_from, &dt_to);
     wallet.print_usd_trades(&dt_from, &dt_to);
-    wallet.print_sell_trades(false, &dt_from, &dt_to);
+    wallet.print_sell_trades(true, &dt_from, &dt_to);
     wallet.print_proceeds(&dt_from, &dt_to);
     wallet.print_holdings();
 }
